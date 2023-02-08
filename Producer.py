6639e5037a3b85e44164c2053851bd8ccc3a2821
nghiaNT2310 <93041270+nghiaNT2310@users.noreply.github.com>
2023-02-08 19:39:03 +0700

    update

--- Producer.py
+++ Producer.py
@@ -23,7 +23,7 @@ for artist in artists:
     print('__________')
     # producer.send(topic_artist, artist)
     id = artist['id']
-    # albums = spotipyGetData.album_by_artists(id)
+    albums = spotipyGetData.album_by_artists(id)
     for album in albums:
         producer.send(topic_album, album)
         id_album = album['id']
